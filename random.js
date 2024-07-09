c5f75b3774121c54d185e56036d2bc39e6146933
Hadixp <hadi.b.2002@gmail.com>
2024-07-09 23:24:45 +0430

    codes fixed

--- random.js
+++ random.js
@@ -1,11 +1,3 @@
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-console.log(getRandom(0, 15));
-
-if (getRandom(0, 15) == 10) {
-    console.log("right");
-}else{
-    console.log('faild');
-}
